071412cfae4620c7e698174c1f3cc4c15062df0b
chirothespearow <ritvikrrao@gmail.com>
2023-06-08 23:54:32 +0530

    Add gradual shading functionality

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+// Functionality yet to be added : add a button to return back to default black color
 
 var size = 16;
 var percent = (1/size)*100 + '%';
 var rainbow = false;
+var gradual = false;
 change_size();
 
 function getSize(){
     reset();
     size = prompt('Enter the new size:');
+    if(size === null){size = 16;}
     percent = (1/size)*100 + '%';
-    console.log(size);
     change_size();
 }
 
 function reset(){
     const chequers = document.querySelectorAll('.chequer');
-    console.log(chequers);
     chequers.forEach(chequer => chequer.remove());
 
 }
@@ -36,6 +37,9 @@ function chooseMode(chequers){
     if(rainbow){
         chequers.forEach(square => square.addEventListener('mouseover',hoverRainbow));
     }
+    else if(gradual){
+        chequers.forEach(square => square.addEventListener('mouseover',gradualHover));
+    }
     else{
         chequers.forEach(square => square.addEventListener('mouseover',hover));
     }
@@ -45,16 +49,31 @@ function hover(event){
     this.classList.add('hover');
 }
 
+function gradualHover(event){
+    this.classList.add('hover');
+    let opacityValue = this.style.opacity;
+    if(opacityValue < 1){
+        opacityValue=Number(opacityValue)+0.1;
+        this.style.opacity = opacityValue;
+    }
+}
 
 function toggleRainbow(){
     rainbow = !rainbow;
+    if(gradual){gradual=false;}
+    reset();
+    change_size();
+}
+
+function toggleGradual(){
+    gradual = !gradual;
+    if(rainbow){rainbow = false;}
     reset();
     change_size();
 }
 
 function hoverRainbow(e){
     let color = Math.floor(Math.random()*16777215).toString(16);
-    console.log(color);
     this.style.backgroundColor = '#' + color;
 }
 
@@ -63,3 +82,6 @@ size_button.addEventListener('click', getSize);
 
 const rainbowButton = document.querySelector('.rainbow');
 rainbowButton.addEventListener('click',toggleRainbow);
+
+const shading = document.querySelector(".gradual");
+shading.addEventListener('click',toggleGradual);
